163c2b2570956902a92b7fb9f6116885d88dbd5e
Malik Muhammad Awan <muhammad.dev.pk@gmail.com>
2026-01-18 23:09:34 +0500

    Update ERC721.ts

--- ignition/modules/ERC721.ts
+++ ignition/modules/ERC721.ts
@@ -1,7 +1,6 @@
 import { buildModule } from "@nomicfoundation/hardhat-ignition/modules";
 
-export default buildModule("ERC721Module",(m) => {
-    const erc721 = m.contract("ERC721",
-        ["MyNFT", "MNFT"]);
-        return {erc721};
-})
+export default buildModule("ERC721Module", (m) => {
+    const erc721 = m.contract("ERC721", ["MyNFT", "MNFT"]);
+    return { erc721 };
+});
